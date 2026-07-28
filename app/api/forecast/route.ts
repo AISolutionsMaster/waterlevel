9e268f4941fba8fb8cbff736e22befcea50f008f
AISolutionsMaster <buianthai.work@gmail.com>
2026-07-28 13:01:49 +1000

    1y

--- app/api/forecast/route.ts
+++ app/api/forecast/route.ts
@@ -371,8 +371,9 @@ export async function GET(request: Request) {
 
   let durationHours = 72;
   if (range === '7d') durationHours = 168;
-  else if(range === '15d') durationHours = 360;
+  else if (range === '15d') durationHours = 360;
   else if (range === '30d') durationHours = 720;
+  else if (range === '1y') durationHours = 8760;
 
   await initDatabaseSchema();
 
